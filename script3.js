318f402eea3152572decc26b3314b89f625143b3
MYGguy <mygguy613@gmail.com>
2022-02-17 17:03:34 -0500

    display each item on new line

--- script3.js
+++ script3.js
@@ -1,14 +1,25 @@
-var x = document.getElementById('the-input');
+var getInput = document.getElementById('the-input');
 
 const listings = [];
 
 function displayOutput() {
-    console.log(x.value);
-    document.getElementById('display').innerHTML = x.value;
+    var addToArray = listings.push(getInput.value);
+    
+    getInput.value = '';
 
-    var addToArray = listings.push(x.value);
-    document.getElementById('display').innerHTML = listings;
-    console.log(listings);
+    console.log('Console Output: ')
+    forLoop();
 
     return;
+};
+
+function forLoop() {
+    let text = '';
+    for (let i = 0; i < listings.length; i++) {
+        text += listings[i] + '</br>';
+        console.log(listings[i]);
+    };
+
+    document.getElementById('display').innerHTML = text;
+
 };
